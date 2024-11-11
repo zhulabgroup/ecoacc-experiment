f1c29d34b11a247dee7476eb43bae08dfe0716f1
Kara Christina Dobson <kcdobson@gl3423.arc-ts.umich.edu>
2024-11-11 11:49:35 -0500

    including amb temps for jrgce

--- 1-clean/JRGCE_raw_to_clean.R
+++ 1-clean/JRGCE_raw_to_clean.R
@@ -13,12 +13,14 @@ library(tidyverse)
 path_abun_data = "/nfs/turbo/seas-zhukai/proj-grassland-cfp/intermediate/observation-experiment/final-community/"
 path_bio_data = "/nfs/turbo/seas-zhukai/datasets/vegetation/JRGCE/JRGCE Harvest1 AGB/"
 path_meta_data = "/nfs/turbo/seas-zhukai/datasets/vegetation/JRGCE/"
+path_temp_data = "/nfs/turbo/seas-zhukai/datasets/vegetation/JRGCE/"
 path_home = "/home/kcdobson"
-setwd(path_abun_data)
+setwd(path_temp_data)
 
 # Read in data
 jrgce_abun_data <- read.csv("jrgce.csv")
 jrgce_meta_data <- read.csv("JRGCE treatments 1998_2014 plus single code for all treatments.csv")
+jrgce_temp_data <- read.csv("JRGCE environmental summary for MIDPOINT, H1, H1, DOY 308_126.csv")
 # Read in all biomass data files at once
 temp = list.files(pattern="\\.csv$")
 for (i in 1:length(temp)) {
@@ -29,7 +31,6 @@ for (i in 1:length(temp)) {
   data$Year <- year
   assign(var_name, data)
 }
-
 # Fixing data to match before merging all files
 X2006 <- X2006[,c(1:10,13)]
 colnames(X2014)[which(names(X2014) == "Species")] <- "name_during_sort"
@@ -49,7 +50,6 @@ colnames(X2010) <- colnames(X2014)
 colnames(X2011) <- colnames(X2014)
 colnames(X2012) <- colnames(X2014)
 colnames(X2013) <- colnames(X2014)
-
 # Merging all biomass files into one
 jrgce_bio_data <- rbind(X1998,X1999,X2000,X2001,X2002,X2003,X2004,X2005,X2006,X2007,X2008,
                         X2009,X2010,X2011,X2012,X2013,X2014)
@@ -66,10 +66,20 @@ colnames(jrgce_bio_data)[which(names(jrgce_bio_data) == "treatmentsummary")] <- 
 colnames(jrgce_abun_data)[which(names(jrgce_abun_data) == "abund")] <- "percent_cover"
 colnames(jrgce_abun_data)[which(names(jrgce_abun_data) == "treat")] <- "treatment"
 
+# Merging with temp data
+jrgce_temp_data <- jrgce_temp_data %>%
+  filter(VARIABLE == "AVG TEMP SEASON START TO MIDPT BETWEEN HARVESTS" &
+           TREATMENT == "AMB")
+jrgce_temp_filtered <- jrgce_temp_data[,c(1,5)]
+colnames(jrgce_temp_filtered)[which(names(jrgce_temp_filtered) == "VALUE")] <- "mean_C_temp_summer"
+colnames(jrgce_temp_filtered)[which(names(jrgce_temp_filtered) == "harvest_year")] <- "year"
+jrgce_abun_data <- left_join(jrgce_abun_data, jrgce_temp_filtered, by="year")
+jrgce_bio_data <- left_join(jrgce_bio_data, jrgce_temp_filtered, by="year")
+
 # Upload data
 path_out = "/nfs/turbo/seas-zhukai/proj-ecoacc/JRGCE/"
 write.csv(jrgce_abun_data,paste(path_out,'jrgce_clean.csv'))
 write.csv(jrgce_bio_data,paste(path_out,'jrgce_ecosystem_dat_clean.csv'))
-s
+
 
 
