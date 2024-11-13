dff9d617af6e54bee90d4521fbede3674197710a
Kara Christina Dobson <kcdobson@gl3098.arc-ts.umich.edu>
2024-11-13 11:44:23 -0500

    combining figs in one script

--- 7-combine_experiments/combined_fig.R
+++ 7-combine_experiments/combined_fig.R
@@ -12,25 +12,34 @@ library(ggpubr)
 
 # Set path to turbo to get data
 path_data = "/nfs/turbo/seas-zhukai/proj-ecoacc/TeRaCON/"
-path_data = "/nfs/turbo/seas-zhukai/proj-ecoacc/JRGCE/"
 setwd(path_data)
-
 # Load in data
 CTI_sens_teracon <- read.csv(" CTI_sens_teracon.csv")
+CTI_teracon <- read.csv(" CTI_teracon.csv")
 NPP_teracon <- read.csv(" eco_response_teracon.csv")
-CTI_sens_jrgce <- read.csv(" CTI_sens_jrgce.csv")
-NPP_jrgce <- read.csv(" eco_response_jrgce.csv")
+CTI_CPI_teracon <- read.csv(" CTI_CPI_teracon.csv")
 
+# Set path to data
+path_data = "/nfs/turbo/seas-zhukai/proj-ecoacc/JRGCE/"
+setwd(path_data)
+# Load in data
+CTI_sens_jrgce <- read.csv(" CTI_sens_jrgce.csv")
+CTI_jrgce <- read.csv(" CTI_jrgce.csv")
+NPP_jrgce <- read.csv(" eco_response_jrgce.csv")
+CTI_CPI_jrgce <- read.csv(" CTI_CPI_jrgce.csv")
+
+
+##### Fig: CTI, ANPP, and ambient temps over time #####
 # CTI figures
 CTI_tera_plot <- ggplot(CTI_sens_teracon, aes(x = year, y = sensitivity)) +
   geom_smooth() +
-  labs(x = "Year", y = "CTI (Warmed - Ambient)") +
+  labs(x = "Year", y = "TeRaCON\nCTI (Warmed - Ambient)") +
   scale_x_continuous(breaks = seq(2012, 2023, by = 2)) +
   theme_bw()
 CTI_jrgce_plot <- ggplot(CTI_sens_jrgce, aes(x = year, y = sensitivity)) +
   geom_smooth() +
-  labs(x = "Year", y = "CTI (Warmed - Ambient)") +
-  scale_x_continuous(breaks = seq(1998, 2014, by = 2)) +
+  labs(x = "Year", y = "JRGCE\nCTI (Warmed - Ambient)") +
+  scale_x_continuous(breaks = seq(1998, 2014, by = 3)) +
   theme_bw()
 
 # Biomass figures
@@ -44,7 +53,7 @@ npp_tera_plot <- ggplot(NPP_teracon, aes(x = year, y = sensitivity)) +
 npp_jrgce_plot <- ggplot(NPP_jrgce, aes(x = year, y = sensitivity)) +
   geom_smooth() +
   labs(x = "Year", y = "Biomass (Warmed - Ambient)") +
-  scale_x_continuous(breaks = seq(1998, 2014, by = 2)) +
+  scale_x_continuous(breaks = seq(1998, 2014, by = 3)) +
   theme_bw()
 
 # Ambient temp change figures
@@ -56,7 +65,7 @@ temp_tera_plot <- ggplot(CTI_sens_teracon, aes(x = year, y = mean_C_temp_summer)
 temp_jrgce_plot <- ggplot(CTI_sens_jrgce, aes(x = year, y = mean_C_temp_summer)) +
   geom_smooth() +
   labs(x = "Year", y = "Ambient temperature (°C)") +
-  scale_x_continuous(breaks = seq(1998, 2014, by = 2)) +
+  scale_x_continuous(breaks = seq(1998, 2014, by = 3)) +
   theme_bw()
 
 # Combine figures into one multi-panel plot
@@ -65,3 +74,68 @@ ggarrange(CTI_tera_plot,npp_tera_plot,temp_tera_plot,
           ncol = 3, nrow=2, common.legend = T, legend = "right")
 
 
+
+##### Fig: Arrows pointing from ambient to warmed for each year #####
+arrow_teracon <- ggplot(CTI_CPI_teracon) +
+  geom_segment(aes(x = CTI_HTamb, y = CPI_HTamb, 
+                   xend = CTI_HTelv, yend = CPI_HTelv,
+                   color = year),
+               arrow = arrow(length = unit(0.1, "inches"))) +
+  geom_point(aes(x = CTI_HTamb, y = CPI_HTamb), color = "black") +
+  geom_point(aes(x = CTI_HTelv, y = CPI_HTelv), color = "red") +
+  labs(x = "CTI", y = "CPI", title = "TeRaCON") +
+  scale_color_viridis_c(option = "magma") +
+  theme_minimal()
+
+arrow_jrgce <- ggplot(CTI_CPI_jrgce) +
+  geom_segment(aes(x = CTI_ambient, y = CPI_ambient, 
+                   xend = CTI_warmed, yend = CPI_warmed,
+                   color = year),
+               arrow = arrow(length = unit(0.1, "inches"))) +
+  geom_point(aes(x = CTI_ambient, y = CPI_ambient), color = "black") +
+  geom_point(aes(x = CTI_warmed, y = CPI_warmed), color = "red") +
+  labs(x = "CTI", y = "CPI", title = "JRGCE") +
+  scale_color_viridis_c(option = "magma") +
+  theme_minimal()
+
+# Combine figures into one multi-panel plot
+ggarrange(arrow_teracon,arrow_jrgce,
+          ncol = 2, nrow=1)
+
+
+
+##### Fig: Mean CTI over time in warmed and ambient #####
+# note: could also change y = CTI to a different metric (CTI_sd, CTI_skew, etc.)
+CTI_teracon_plot <- ggplot(CTI_teracon, aes(x = year, y = CTI, color = temp_treatment, group=temp_treatment)) +
+  geom_jitter(alpha = 0.1,
+              position = position_jitterdodge(dodge.width = 0.7)) +  # Add jittered points
+  stat_summary(fun = mean,
+               fun.min = mean,
+               fun.max = mean,
+               geom = "crossbar",
+               width = 0.4,
+               position = position_dodge(width = 0.7),
+               aes(color = temp_treatment, group = temp_treatment)) +
+  labs(title="TeRaCON") +
+  theme_minimal() +
+  scale_color_manual(values = c("HTamb" = "blue", "HTelv" = "red"))
+
+CTI_jrgce <- CTI_jrgce %>%
+  filter(!(year == 1998))
+CTI_jrgce_plot <- ggplot(CTI_jrgce, aes(x = year, y = CTI, color = temp_treatment, group=temp_treatment)) +
+  geom_jitter(alpha = 0.1,
+              position = position_jitterdodge(dodge.width = 0.7)) +  # Add jittered points
+  stat_summary(fun = mean,
+               fun.min = mean,
+               fun.max = mean,
+               geom = "crossbar",
+               width = 0.4,
+               position = position_dodge(width = 0.7),
+               aes(color = temp_treatment, group = temp_treatment)) +
+  labs(title = "JRGCE") +
+  theme_minimal() +
+  scale_color_manual(values = c("ambient" = "blue", "warmed" = "red"))
+
+# Combine figures into one multi-panel plot
+ggarrange(CTI_teracon_plot,CTI_jrgce_plot,
+          ncol = 2, nrow=1)
