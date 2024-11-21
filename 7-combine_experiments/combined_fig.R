6f9203de7bbd0339f4e0c739ffad95b19fcaf670
Kara Christina Dobson <kcdobson@gl3171.arc-ts.umich.edu>
2024-11-21 10:17:24 -0500

    combining phace w/ other experiments

--- 7-combine_experiments/combined_fig.R
+++ 7-combine_experiments/combined_fig.R
@@ -18,6 +18,7 @@ setwd(path_data)
 CTI_sens_teracon <- read.csv(" CTI_sens_teracon.csv")
 CTI_teracon <- read.csv(" CTI_teracon.csv")
 NPP_teracon <- read.csv(" eco_response_teracon.csv")
+NPP_overall_teracon <- read.csv(" eco_response_overall_teracon.csv")
 CTI_CPI_teracon <- read.csv(" CTI_CPI_teracon.csv")
 # Data using ecoregion 8 spp occurrences from GBIF + year-round MAT:
 CTI_sens_teracon_lim <- read.csv(" CTI_sens_teracon_limited.csv")
@@ -27,6 +28,10 @@ CTI_CPI_teracon_lim <- read.csv(" CTI_CPI_teracon_limited.csv")
 CTI_sens_teracon_6month <- read.csv(" CTI_sens_6month_teracon_limited.csv")
 CTI_teracon_6month <- read.csv(" CTI_6month_teracon_limited.csv")
 CTI_CPI_teracon_6month <- read.csv(" CTI_CPI_6month_teracon_limited.csv")
+# Data w/o big bluestem:
+CTI_sens_teracon_noblue <- read.csv(" CTI_sens_teracon_nobluestem.csv")
+CTI_teracon_noblue <- read.csv(" CTI_teracon_nobluestem.csv")
+CTI_CPI_teracon_noblue <- read.csv(" CTI_CPI_teracon_nobluestem.csv")
 
 # Set path to data
 path_data = "/nfs/turbo/seas-zhukai/proj-ecoacc/JRGCE/"
@@ -35,7 +40,18 @@ setwd(path_data)
 CTI_sens_jrgce <- read.csv(" CTI_sens_jrgce.csv")
 CTI_jrgce <- read.csv(" CTI_jrgce.csv")
 NPP_jrgce <- read.csv(" eco_response_jrgce.csv")
+NPP_overall_jrgce <- read.csv(" eco_response_overall_jrgce.csv")
 CTI_CPI_jrgce <- read.csv(" CTI_CPI_jrgce.csv")
+
+# Set path to data
+path_data = "/nfs/turbo/seas-zhukai/proj-ecoacc/PHACE/"
+setwd(path_data)
+# Load in data
+CTI_sens_phace <- read.csv(" CTI_sens_phace_limited.csv")
+CTI_phace <- read.csv(" CTI_phace_limited.csv")
+NPP_phace <- read.csv(" eco_response_phace.csv")
+NPP_overall_phace <- read.csv(" eco_response_overall_phace.csv")
+CTI_CPI_phace <- read.csv(" CTI_CPI_phace_limited.csv")
 
 
 ##### Fig: CTI, ANPP, and ambient temps over time #####
@@ -50,6 +66,11 @@ CTI_jrgce_plot <- ggplot(CTI_sens_jrgce, aes(x = year, y = sensitivity)) +
   labs(x = "Year", y = "JRGCE\nCTI (Warmed - Ambient)") +
   scale_x_continuous(breaks = seq(1998, 2014, by = 3)) +
   theme_bw()
+CTI_phace_plot <- ggplot(CTI_sens_phace, aes(x = year, y = sensitivity)) +
+  geom_smooth() +
+  labs(x = "Year", y = "PHACE\nCTI (Warmed - Ambient)") +
+  scale_x_continuous(breaks = seq(2007, 2013)) +
+  theme_bw()
 
 # Biomass figures
 NPP_teracon <- NPP_teracon %>%
@@ -63,6 +84,11 @@ npp_jrgce_plot <- ggplot(NPP_jrgce, aes(x = year, y = sensitivity)) +
   geom_smooth() +
   labs(x = "Year", y = "Biomass (Warmed - Ambient)") +
   scale_x_continuous(breaks = seq(1998, 2014, by = 3)) +
+  theme_bw()
+npp_phace_plot <- ggplot(NPP_phace, aes(x = year, y = sensitivity)) +
+  geom_smooth() +
+  labs(x = "Year", y = "Biomass (Warmed - Ambient)") +
+  scale_x_continuous(breaks = seq(2007, 2013)) +
   theme_bw()
 
 # Ambient temp change figures
@@ -80,7 +106,43 @@ temp_jrgce_plot <- ggplot(CTI_sens_jrgce, aes(x = year, y = mean_C_temp_summer))
 # Combine figures into one multi-panel plot
 ggarrange(CTI_tera_plot,npp_tera_plot,temp_tera_plot,
           CTI_jrgce_plot,npp_jrgce_plot,temp_jrgce_plot,
-          ncol = 3, nrow=2, common.legend = T, legend = "right")
+          CTI_phace_plot,npp_phace_plot,
+          ncol = 3, nrow=3, common.legend = T, legend = "right")
+
+
+
+##### Fig: correlating biomass w/ CTI
+# TeRaCON
+CTI_yearly_avg_tera <- CTI_teracon_6month %>%
+  group_by(year,temp_treatment) %>%
+  summarize(mean_CTI = mean(CTI))
+NPP_CTI_teracon <- left_join(CTI_yearly_avg_tera, NPP_overall_teracon, by=c("year","temp_treatment"))
+tera_scatter <- ggscatter(NPP_CTI_teracon, x = "mean_ab_bio", y = "mean_CTI", 
+          add = "reg.line", conf.int = TRUE, 
+          cor.coef = TRUE, cor.method = "pearson",
+          xlab = "Biomass", ylab = "TeRaCON\nCTI")
+# JRGCE
+CTI_yearly_avg_jrgce <- CTI_jrgce %>%
+  group_by(year,temp_treatment) %>%
+  summarize(mean_CTI = mean(CTI))
+NPP_CTI_jrgce <- left_join(CTI_yearly_avg_jrgce, NPP_overall_jrgce, by=c("year","temp_treatment"))
+jrgce_scatter <- ggscatter(NPP_CTI_jrgce, x = "mean_ab_bio", y = "mean_CTI", 
+          add = "reg.line", conf.int = TRUE, 
+          cor.coef = TRUE, cor.method = "pearson",
+          xlab = "Biomass", ylab = "JRGCE\nCTI")
+# PHACE
+CTI_yearly_avg_phace <- CTI_phace %>%
+  group_by(year,temp_treatment) %>%
+  summarize(mean_CTI = mean(CTI))
+NPP_CTI_phace <- left_join(CTI_yearly_avg_phace, NPP_overall_phace, by=c("year","temp_treatment"))
+phace_scatter <- ggscatter(NPP_CTI_phace, x = "mean_ab_bio", y = "mean_CTI", 
+                           add = "reg.line", conf.int = TRUE, 
+                           cor.coef = TRUE, cor.method = "pearson",
+                           xlab = "Biomass", ylab = "PHACE\nCTI")
+
+# Combine into one figure
+ggarrange(tera_scatter,jrgce_scatter,phace_scatter,
+          nrow=3)
 
 
 
@@ -107,15 +169,26 @@ arrow_jrgce <- ggplot(CTI_CPI_jrgce) +
   scale_color_viridis_c(option = "magma") +
   theme_minimal()
 
+arrow_phace <- ggplot(CTI_CPI_phace) +
+  geom_segment(aes(x = CTI_ambient, y = CPI_ambient, 
+                   xend = CTI_warmed, yend = CPI_warmed,
+                   color = year),
+               arrow = arrow(length = unit(0.1, "inches"))) +
+  geom_point(aes(x = CTI_ambient, y = CPI_ambient), color = "black") +
+  geom_point(aes(x = CTI_warmed, y = CPI_warmed), color = "red") +
+  labs(x = "CTI", y = "CPI", title = "PHACE") +
+  scale_color_viridis_c(option = "magma") +
+  theme_minimal()
+
 # Combine figures into one multi-panel plot
-ggarrange(arrow_teracon,arrow_jrgce,
-          ncol = 2, nrow=1)
+ggarrange(arrow_teracon,arrow_jrgce,arrow_phace,
+          ncol = 3, nrow=1)
 
 
 
 ##### Fig: Mean CTI over time in warmed and ambient #####
 # note: could also change y = CTI to a different metric (CTI_sd, CTI_skew, etc.)
-CTI_teracon_plot <- ggplot(CTI_teracon, aes(x = year, y = CTI_sd, color = temp_treatment, group=temp_treatment)) +
+CTI_teracon_plot <- ggplot(CTI_teracon, aes(x = year, y = CTI, color = temp_treatment, group=temp_treatment)) +
   geom_jitter(alpha = 0.1,
               position = position_jitterdodge(dodge.width = 0.7)) +  # Add jittered points
   stat_summary(fun = mean,
@@ -131,7 +204,7 @@ CTI_teracon_plot <- ggplot(CTI_teracon, aes(x = year, y = CTI_sd, color = temp_t
 
 CTI_jrgce <- CTI_jrgce %>%
   filter(!(year == 1998))
-CTI_jrgce_plot <- ggplot(CTI_jrgce, aes(x = year, y = CTI_sd, color = temp_treatment, group=temp_treatment)) +
+CTI_jrgce_plot <- ggplot(CTI_jrgce, aes(x = year, y = CTI, color = temp_treatment, group=temp_treatment)) +
   geom_jitter(alpha = 0.1,
               position = position_jitterdodge(dodge.width = 0.7)) +  # Add jittered points
   stat_summary(fun = mean,
@@ -145,13 +218,27 @@ CTI_jrgce_plot <- ggplot(CTI_jrgce, aes(x = year, y = CTI_sd, color = temp_treat
   theme_minimal() +
   scale_color_manual(values = c("ambient" = "blue", "warmed" = "red"))
 
+CTI_phace_plot <- ggplot(CTI_phace, aes(x = year, y = CTI, color = temp_treatment, group=temp_treatment)) +
+  geom_jitter(alpha = 0.1,
+              position = position_jitterdodge(dodge.width = 0.7)) +  # Add jittered points
+  stat_summary(fun = mean,
+               fun.min = mean,
+               fun.max = mean,
+               geom = "line",
+               #width = 0.4,
+               position = position_dodge(width = 0.7),
+               aes(color = temp_treatment, group = temp_treatment)) +
+  labs(title = "PHACE") +
+  theme_minimal() +
+  scale_color_manual(values = c("ambient" = "blue", "warmed" = "red"))
+
 # Combine figures into one multi-panel plot
-ggarrange(CTI_teracon_plot,CTI_jrgce_plot,
-          ncol = 2, nrow=1)
+ggarrange(CTI_teracon_plot,CTI_jrgce_plot,CTI_phace_plot,
+          ncol = 3, nrow=1)
 
 
 
-##### Fig: comparing global GBIF occurrence data w/ ecoregion 8 GBIF occurrence data
+##### Fig: teracon --> comparing global GBIF occurrence data w/ ecoregion 8 GBIF occurrence data
 CTI_global_tera <- ggplot(CTI_teracon, aes(x = year, y = CTI, color = temp_treatment, group=temp_treatment)) +
   geom_jitter(alpha = 0.1,
               position = position_jitterdodge(dodge.width = 0.7)) +  # Add jittered points
@@ -218,7 +305,7 @@ ggarrange(CTI_global_tera, arrow_global, CTI_global_smooth,
 
 
 
-##### Fig: comparing year-round temps w/ 6-month (March-Aug) temps for niche calculation
+##### Fig: teracon --> comparing year-round temps w/ 6-month (March-Aug) temps for niche calculation
 CTI_yearround_tera <- ggplot(CTI_teracon_lim, aes(x = year, y = CTI, color = temp_treatment, group=temp_treatment)) +
   geom_jitter(alpha = 0.1,
               position = position_jitterdodge(dodge.width = 0.7)) +  # Add jittered points
@@ -285,5 +372,76 @@ CTI_6month_smooth <- ggplot(CTI_sens_teracon_6month, aes(x = year, y = sensitivi
 # Combine figures into one multi-panel plot
 ggarrange(CTI_yearround_tera, arrow_yearround, CTI_yearround_smooth,
           CTI_6month_tera, arrow_6month, CTI_6month_smooth,
+          ncol = 3, nrow=2)
+
+
+
+##### Fig: teracon --> w/ and w/o big bluestem
+CTI_blue_tera <- ggplot(CTI_teracon_6month, aes(x = year, y = CTI, color = temp_treatment, group=temp_treatment)) +
+  geom_jitter(alpha = 0.1,
+              position = position_jitterdodge(dodge.width = 0.7)) +  # Add jittered points
+  stat_summary(fun = mean,
+               fun.min = mean,
+               fun.max = mean,
+               geom = "line",
+               # width = 0.4,
+               position = position_dodge(width = 0.7),
+               aes(color = temp_treatment, group = temp_treatment)) +
+  ylim(7,22) +
+  labs(title="w/ big bluestem") +
+  theme_minimal() +
+  scale_color_manual(values = c("HTamb" = "blue", "HTelv" = "red"))
+CTI_noblue_tera <- ggplot(CTI_teracon_noblue, aes(x = year, y = CTI, color = temp_treatment, group=temp_treatment)) +
+  geom_jitter(alpha = 0.1,
+              position = position_jitterdodge(dodge.width = 0.7)) +  # Add jittered points
+  stat_summary(fun = mean,
+               fun.min = mean,
+               fun.max = mean,
+               geom = "line",
+               # width = 0.4,
+               position = position_dodge(width = 0.7),
+               aes(color = temp_treatment, group = temp_treatment)) +
+  ylim(7,22) +
+  labs(title="w/o big bluestem") +
+  theme_minimal() +
+  scale_color_manual(values = c("HTamb" = "blue", "HTelv" = "red"))
+
+arrow_blue <- ggplot(CTI_CPI_teracon_6month) +
+  geom_segment(aes(x = CTI_HTamb, y = CPI_HTamb, 
+                   xend = CTI_HTelv, yend = CPI_HTelv,
+                   color = year),
+               arrow = arrow(length = unit(0.1, "inches"))) +
+  geom_point(aes(x = CTI_HTamb, y = CPI_HTamb), color = "black") +
+  geom_point(aes(x = CTI_HTelv, y = CPI_HTelv), color = "red") +
+  labs(x = "CTI", y = "CPI", title = "w/ big bluestem") +
+  scale_color_viridis_c(option = "magma") +
+  theme_minimal()
+arrow_noblue <- ggplot(CTI_CPI_teracon_noblue) +
+  geom_segment(aes(x = CTI_HTamb, y = CPI_HTamb, 
+                   xend = CTI_HTelv, yend = CPI_HTelv,
+                   color = year),
+               arrow = arrow(length = unit(0.1, "inches"))) +
+  geom_point(aes(x = CTI_HTamb, y = CPI_HTamb), color = "black") +
+  geom_point(aes(x = CTI_HTelv, y = CPI_HTelv), color = "red") +
+  labs(x = "CTI", y = "CPI", title = "w/o big bluestem") +
+  scale_color_viridis_c(option = "magma") +
+  theme_minimal()
+
+CTI_blue_smooth <- ggplot(CTI_sens_teracon_6month, aes(x = year, y = sensitivity)) +
+  geom_smooth() +
+  labs(x = "Year", y = "CTI (Warmed - Ambient)",title = "w/ big bluestem") +
+  scale_x_continuous(breaks = seq(2012, 2023, by = 2)) +
+  ylim(-0.3,0.3) +
+  theme_bw()
+CTI_noblue_smooth <- ggplot(CTI_sens_teracon_noblue, aes(x = year, y = sensitivity)) +
+  geom_smooth() +
+  labs(x = "Year", y = "CTI (Warmed - Ambient)",title = "w/o big bluestem") +
+  scale_x_continuous(breaks = seq(2012, 2023, by = 2)) +
+  ylim(-0.3,0.3) +
+  theme_bw()
+
+# Combine figures into one multi-panel plot
+ggarrange(CTI_blue_tera, arrow_blue, CTI_blue_smooth,
+          CTI_noblue_tera, arrow_noblue, CTI_noblue_smooth,
           ncol = 3, nrow=2)
 
